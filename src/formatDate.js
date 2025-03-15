88b52225434623779ad1e887c2615ea7d69d6789
Jeny Berezhna <JenySmails24@gmail.com>
2025-03-15 20:52:59 +0000

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -7,8 +7,59 @@
  *
  * @returns {string}
  */
+
+/**
+ * @param {string} date
+ * @param {string[]} fromFormat
+ * @param {string[]} toFormat
+ *
+ * @returns {string}
+ */
+/**
+ * @param {string} date
+ * @param {string[]} fromFormat
+ * @param {string[]} toFormat
+ *
+ * @returns {string}
+ */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let separator = '';
+
+  const separators = ['/', '.', '-'];
+
+  separators.forEach((sep) => {
+    if (date.includes(sep)) {
+      separator = sep;
+    }
+  });
+
+  const dateParts = date.split(separator);
+
+  const dateMap = {};
+
+  fromFormat.forEach((format, index) => {
+    dateMap[format] = dateParts[index];
+  });
+
+  let newDate = '';
+
+  for (let i = 0; i < toFormat.length; i++) {
+    const format = toFormat[i];
+
+    if (format === 'YYYY') {
+      const year = dateMap['YY'];
+
+      newDate += (year < 30 ? '20' : '19') + year;
+    } else {
+      newDate += dateMap[format];
+    }
+
+    if (i < toFormat.length - 1) {
+      newDate += separator;
+    }
+  }
+
+  return newDate;
 }
 
 module.exports = formatDate;
